bfb64f3cc800f42bd578f3e51595cce94ebd90c7
yvonne <yvonne@eyesnareinc.com>
2025-05-14 19:36:58 -0400

    updated files working

---
+++ open-insecure-chrome.sh
@@ -0,0 +1,2 @@
+echo '"/mnt/c/Program Files/Google/Chrome/Application/chrome.exe" --disable-web-security --user-data-dir="C:/temp/chrome" https://localhost:4000 https://hubs.local:8080 https://hubs.local:9090' > ~/open-insecure-chrome.sh
+chmod +x ~/open-insecure-chrome.sh
